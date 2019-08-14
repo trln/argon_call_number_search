b4366810d51948f8e6628025fd41ba06dc8f7cd9
Cory Lown <cory.lown@duke.edu>
2019-08-14 09:53:08 -0400

    rm cruft

--- spec/argon_call_number_search/configurable_spec.rb
+++ spec/argon_call_number_search/configurable_spec.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.describe ArgonCallNumberSearch::Configurable do
-  describe 'default values' do
-    it 'has a default value for acc_num_without_space' do
-      expect(ArgonCallNumberSearch.acc_num_without_space).to(
-        eq('^[\s\d[[:punct:]]]*(CD|DVD|LP|AV|VC|LD)([A-Z\d]+)')
-      )
-    end
+  it 'has a default value for acc_num_without_space' do
+    expect(ArgonCallNumberSearch.acc_num_without_space).to(
+      eq('^[\s\d[[:punct:]]]*(CD|DVD|LP|AV|VC|LD)([A-Z\d]+)')
+    )
+  end
 
-    it 'has a default value for acc_num_with_space' do
-      expect(ArgonCallNumberSearch.acc_num_with_space).to(
-        eq('^[\s\d[[:punct:]]]*(CD|DVD|LP|AV|VC|LD)(\s+)([A-Z\d]+)')
-      )
-    end
+  it 'has a default value for acc_num_with_space' do
+    expect(ArgonCallNumberSearch.acc_num_with_space).to(
+      eq('^[\s\d[[:punct:]]]*(CD|DVD|LP|AV|VC|LD)(\s+)([A-Z\d]+)')
+    )
   end
 end
