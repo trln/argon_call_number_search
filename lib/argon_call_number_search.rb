ff92e87a633a7922473b61422c7c9878bb577b46
kazymovae <kazymovae@gmail.com>
2023-08-03 15:23:40 -0400

    Truncate call number at 50 terms

--- lib/argon_call_number_search.rb
+++ lib/argon_call_number_search.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'argon_call_number_search/version'
+require 'lcsort'
 
 module ArgonCallNumberSearch
   autoload :Configurable, 'argon_call_number_search/configurable'
